fdcebca96a0c3440aabb4ab80496b00141561c5b
Muhsinullah <muhsinkhanj786@gmail.com>
2024-11-07 21:34:47 +0500

    aded
    aded

---
+++ components/Navbar.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Image from "next/image";
+import logo from "../public/logo.png"; // Replace with your logo path
+
+const Navbar: React.FC = () => {
+  return (
+    <nav className="bg-[#043873] tracking-normal text-white py-3 px-6 flex justify-between items-center pl-36 pr-36 ">
+      <div className="flex space-x-2"><Image className='text-7xl' src={logo} alt="Logo" width={138} height={160} /> {/* Logo Image */}</div>
+      <ul className="flex space-x-6">
+        <li className='text-xs py-2'>Products</li>
+        <li className='text-xs py-2'>Solutions</li>
+        <li className='text-xs py-2'>Resources</li>
+        <li className='text-xs py-2 pr-28'>Pricing</li>
+        <li>
+          <button className="text-xs bg-secondary text-black px-7 py-3  rounded">Login</button>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
